621e28d74410fb6b10f60c1d7294f0c45a92caaa
t2 <oo_ri@naver.com>
2021-03-29 16:21:38 +0900

    The Movie API Setup and URL, Parameter Update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,13 +2,26 @@ import axios from 'axios';
 
 import API_KEY from './apiKey';
 
-interface IMakeRequest {
-  path: string;
-  params: {};
-}
-
-export const makeRequest = ({ path, params }: IMakeRequest) => {
+const makeRequest = (path: string, params?: {}) => {
   return axios.get(`https://api.themoviedb.org/3${path}`, {
-    params: { api_key: API_KEY, ...params },
+    params: { api_key: API_KEY, language: 'ko-KR', region: 'kr', ...params },
   });
+};
+
+export const movieApi = {
+  discover: () => makeRequest('/discover/movie'),
+  details: (movieId: number) => makeRequest(`/movie/${movieId}`),
+  nowPlaying: () => makeRequest('/movie/now_playing'),
+  popular: () => makeRequest('/movie/popular'),
+  upcoming: () => makeRequest('/movie/upcoming'),
+  search: (query: string) => makeRequest('/search/movie', { query }),
+};
+
+export const tvApi = {
+  details: (tvId: number) => makeRequest(`/tv/${tvId}`),
+  today: () => makeRequest('/tv/airing_today'),
+  thisWeek: () => makeRequest('/tv/on_the_air'),
+  popular: () => makeRequest('/tv/popular'),
+  topRated: () => makeRequest('/tv/top_rated'),
+  search: (query: string) => makeRequest('/search/tv', { query }),
 };
