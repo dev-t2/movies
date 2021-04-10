ed6fe703adfb69ea0ed202ac4b8e97b178eb6f13
t2 <oo_ri@naver.com>
2021-04-11 03:03:29 +0900

    Discovery Screen Image Move Update

--- src/screens/Discovery.tsx
+++ src/screens/Discovery.tsx
@@ -1,5 +1,5 @@
-import React, { memo, useCallback, useEffect, useState } from 'react';
-import { PanResponder, useWindowDimensions } from 'react-native';
+import React, { memo, useCallback, useEffect, useMemo, useState } from 'react';
+import { Animated, PanResponder, useWindowDimensions } from 'react-native';
 import styled from 'styled-components/native';
 
 import { movieApi } from '../lib/api';
@@ -14,12 +14,14 @@ const StyledContainer = styled.View({
 
 interface IStyledCard {
   height: number;
+  zIndex: number;
 }
 
-const StyledCard = styled.View<IStyledCard>(({ height }) => ({
+const StyledCard = styled(Animated.View)<IStyledCard>(({ height, zIndex }) => ({
   width: '90%',
   height: height / 1.3,
   position: 'absolute',
+  zIndex,
 }));
 
 const Discovery = () => {
@@ -29,6 +31,38 @@ const Discovery = () => {
     discover: [{ id: 0, poster_path: '' }],
     error: null,
   });
+  const [topIndex, setTopIndex] = useState(0);
+
+  const position = useMemo(() => new Animated.ValueXY(), []);
+
+  const panResponder = useMemo(
+    () =>
+      PanResponder.create({
+        onStartShouldSetPanResponder: () => true,
+        onPanResponderMove: (event, { dx, dy }) => {
+          position.setValue({ x: dx, y: dy });
+        },
+        onPanResponderRelease: () => {
+          Animated.spring(position, {
+            useNativeDriver: true,
+            toValue: { x: 0, y: 0 },
+          }).start();
+        },
+      }),
+    [position]
+  );
+
+  const transform = useMemo(() => {
+    const rotate = position.x.interpolate({
+      inputRange: [-100, 0, 100],
+      outputRange: ['-5deg', '0deg', '5deg'],
+      extrapolate: 'clamp',
+    });
+
+    return {
+      transform: [{ rotate }, ...position.getTranslateTransform()],
+    };
+  }, [position]);
 
   const getData = useCallback(async () => {
     const [discover, error] = await movieApi.discover();
@@ -40,24 +74,21 @@ const Discovery = () => {
     getData();
   }, [getData]);
 
-  const panResponder = PanResponder.create({
-    onStartShouldSetPanResponder: () => true,
-    onPanResponderMove: (event, { dx }) => {
-      console.log({ dx });
-    },
-  });
-
   return (
     <StyledContainer>
-      {discovery.discover.reverse().map(cover => (
-        <StyledCard
-          key={cover.id}
-          height={height}
-          {...panResponder.panHandlers}
-        >
-          <Poster poster={cover.poster_path} borderRadius={16} />
-        </StyledCard>
-      ))}
+      {discovery.discover.map((cover, index) => {
+        return (
+          <StyledCard
+            key={cover.id}
+            height={height}
+            zIndex={index === topIndex ? 1 : -index}
+            style={index === topIndex ? transform : null}
+            {...panResponder.panHandlers}
+          >
+            <Poster poster={cover.poster_path} borderRadius={16} />
+          </StyledCard>
+        );
+      })}
     </StyledContainer>
   );
 };
