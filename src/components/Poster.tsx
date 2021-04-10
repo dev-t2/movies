9dd65399a847baf37663aca84fb4eecc5566c0d2
t2 <oo_ri@naver.com>
2021-04-11 00:15:03 +0900

    Discover Screen Update

--- src/components/Poster.tsx
+++ src/components/Poster.tsx
@@ -3,19 +3,24 @@ import styled from 'styled-components/native';
 
 import { getImageUri } from '../lib/api';
 
-const StyledImage = styled.Image({
+interface IStyledImage {
+  borderRadius: number;
+}
+
+const StyledImage = styled.Image<IStyledImage>(({ borderRadius }) => ({
   flex: 1,
-  borderRadius: 4,
-});
+  borderRadius,
+}));
 
 interface IPoster {
   poster: string;
+  borderRadius?: number;
 }
 
-const Poster: FC<IPoster> = ({ poster }) => {
+const Poster: FC<IPoster> = ({ poster, borderRadius = 4 }) => {
   const source = useMemo(() => ({ uri: getImageUri(poster) }), [poster]);
 
-  return <StyledImage source={source} />;
+  return <StyledImage source={source} borderRadius={borderRadius} />;
 };
 
 export default memo(Poster);
--- src/screens/Discovery.tsx
+++ src/screens/Discovery.tsx
@@ -1,11 +1,32 @@
 import React, { memo, useCallback, useEffect, useState } from 'react';
-import { Text, View } from 'react-native';
+import { useWindowDimensions } from 'react-native';
+import styled from 'styled-components/native';
+import { Poster } from '../components';
 
 import { movieApi } from '../lib/api';
 
+const StyledContainer = styled.View({
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: '#000',
+});
+
+interface IStyledCard {
+  height: number;
+}
+
+const StyledCard = styled.View<IStyledCard>(({ height }) => ({
+  width: '90%',
+  height: height / 1.3,
+  position: 'absolute',
+}));
+
 const Discovery = () => {
+  const { height } = useWindowDimensions();
+
   const [discovery, setDiscovery] = useState({
-    discover: [],
+    discover: [{ id: 0, poster_path: '' }],
     error: null,
   });
 
@@ -20,10 +41,13 @@ const Discovery = () => {
   }, [getData]);
 
   return (
-    <View>
-      <Text style={{ color: '#000' }}>{discovery.discover.length}</Text>
-      <Text style={{ color: '#000' }}>{discovery.error}</Text>
-    </View>
+    <StyledContainer>
+      {discovery.discover.reverse().map(cover => (
+        <StyledCard key={cover.id} height={height}>
+          <Poster poster={cover.poster_path} borderRadius={16} />
+        </StyledCard>
+      ))}
+    </StyledContainer>
   );
 };
 
